0dc86b61a54d318d55478aec5dcf73db87808f2a
Mhjba <mahjoubaaitalla15@gmail.com>
2024-04-15 16:39:36 +0000

    challenge

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -8,7 +8,7 @@ class Square():
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """ Init class """
+        """ Instantiation of class """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -21,12 +21,12 @@ class Square():
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-
+        """ Printable representation """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-
+    """ Create a square object """
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
